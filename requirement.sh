279ec729bd5fe713dc5e715fe302b7e8e306e2d8
Abhishek Deshmukh <33414377+AbhiiDeshmukh@users.noreply.github.com>
2020-05-08 16:31:08 +0530

    Create requirement.sh

---
+++ requirement.sh
@@ -0,0 +1,10 @@
+# Requests is an elegant and simple HTTP library for Python
+pip install requests
+
+# Beautiful Soup is a library that makes it easy to scrape information from web pages.
+pip install beautifulsoup4
+
+# Encapsulation of date/time values.
+pip install DateTime
+
+# shutil and os libraries.
